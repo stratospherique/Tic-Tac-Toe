bb74b6ded7e9534f7c10ea08982a15a1f27498bd
Jorge Fernando Rincón Figueroa <jofer86@gmail.com>
2019-04-16 18:16:06 -0500

    working on game logic

--- lib/logic.rb
+++ lib/logic.rb
@@ -5,11 +5,22 @@ class Game
 
   def check_winner(board)
     for i in (1..5)
-      next if i%2==0
+      next if i % 2 == 0
       for j in (1..5)
-       @real_grid[i-1][j-1]=board[i][j] if j%2!=0
+        @real_grid[i - 1][j - 1] = board[i][j] if j % 2 != 0
+        end
       end
     end
-    
   end
+  return "player 1 wins!!" if horizontal(@real_grid, "X") 
+  end 
+
+  def horizontal(real, str)
+    if real[0].all? str || real[1].all? str || real[2].all? str
+  end
+
+
+
+
+
 end
