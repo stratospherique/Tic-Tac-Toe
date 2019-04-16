75e018bc571182cf87bb28fe6ac2224d732ac326
stratospheriquegit config --global user.name stratospheriquegit config --global user.name stratospherique <ahmedosm10@gmail.com>
2019-04-17 00:39:31 +0100

    add game logic to determine the winner

--- lib/logic.rb
+++ lib/logic.rb
@@ -12,15 +12,33 @@ class Game
       end
     end
   end
-  return "player 1 wins!!" if horizontal(@real_grid, "X") 
-  end 
+  return "player 1 wins!!" if horizontal(@real_grid, "X") || vertical(@real_grid,"X") || diagonal(@real_grid,"X")
+  return "player 2 wins!!" if horizontal(@real_grid, "O") || vertical(@real_grid,"O") || diagonal(@real_grid,"O")
+  return if 
+  return "draw" 
 
   def horizontal(real, str)
-    if real[0].all? str || real[1].all? str || real[2].all? str
+    (real[0].all? str) || (real[1].all? str) || (real[2].all? str)
   end
 
+  def vertical(real, str)
+    for j in (0..2)
+      for i in (0...2)
+        if real[i][j]!=real[i+1][j] && real[i][j]==str
+          return false
+        end
+      end
+    end
+    true
+  end
 
-
-
+  def diagonal(real, str)
+    res=true
+    for i in (0...2)
+      return false if (real[i][i]!=real[i+1][i+1]) && real[i][i]=str
+      return false if (real[2-i][i]!=real[2-i+1][i+1]) && real[i][i]=str
+    end
+    
+  end
 
 end
