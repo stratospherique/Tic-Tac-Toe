7c7d5adcc948990d584a4b118d95e6f450407373
stratospheriquegit config --global user.name stratospheriquegit config --global user.name stratospherique <ahmedosm10@gmail.com>
2019-04-29 18:50:48 +0100

    add test cases for different scenarios of winning conditions

--- spec/rules_spec.rb
+++ spec/rules_spec.rb
@@ -1,11 +1,40 @@
-require "../bin/rules.rb"
+require './bin/rules.rb'
 
-describe "GameRules" do
+include GameRules
+
+describe GameRules do
   let(:board) { (1..9).to_a }
-  
   describe "#checkwin" do
     it "Should return winner if the first horizontal is filled" do
-      expect(checkwin(["X", "X", "X", 3, 4, 5, 6, 7, 8, 9], "X")).to eq(true)
+      expect(checkwin(["X", "X", "X", 4, 5, 6, 7, 8, 9], "X")).to eq(true)
+    end
+
+    it "Should return false if the first horizontal is not filled with the same symbol" do
+      expect(checkwin(["X", "O", "X", 4, 5, 6, 7, 8, 9], "X")).to eq(false)
+    end
+  
+    it "Should return winner if the first diagonal is filled with the same symbol" do
+      expect(checkwin(["X", "O", "X", 3, "X", 5, 6, 7, "X"], "X")).to eq(true)
+    end
+
+    it "Should return winner if the second diagonal is filled with the same symbol" do
+      expect(checkwin(["O", "X", "O", 3, "O", 5, "O", 7, "X"], "O")).to eq(true)
+    end
+
+    it "Should return winner if the second Horizental row is filled with the same symbol" do
+      expect(checkwin(["X", "O", "X", "O", "O", "O", "X", 8, 9], "O")).to eq(true)
+    end
+
+    it "Should return winner if the third Horizental row is filled with the same symbol" do
+      expect(checkwin(["O", "O", "X", "O", "X", "O", "X", "X", "X"], "X")).to eq(true)
+    end
+
+    it "Should return winner if the first column is filled with the same symbol" do
+      expect(checkwin(["X", "O", "X", "X", 5, 6, "X", 8, 9], "X")).to eq(true)
+    end
+
+    it "Should return winner if the second column is filled with the same symbol" do
+      expect(checkwin(["X", "O", "X", "O", "O", 6, "X", "O", 9], "O")).to eq(true)
     end
   end
 end
